4c376317831f6487f55b0d802e94cd6c38564b7b
fajar-dev <fajarrivaldi2015@gmail.com>
2025-01-16 15:07:15 +0700

    init: bootstraping laravel 11

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation error occurred',
+                'error' => $validator->errors(),
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = User::where('username', $request->username)->where('password', md5($request->password))->first();
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid username or password',
+                'data' => null
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'success' => true,
+            'message' => 'User logged in successfully',
+            'data' => [
+                'access_token' => $token,
+                'token_type' => 'Bearer'
+            ]
+        ], Response::HTTP_OK);
+    }
+
+    public function me()
+    {
+        $data = User::where('id', Auth::user()->id)->first();
+        return response()->json([
+            'success' => true,
+            'message' => 'User recent retrived successfully',
+            'data' => $data
+        ], Response::HTTP_OK);
+    }
+}
